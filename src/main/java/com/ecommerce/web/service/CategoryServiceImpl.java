25e4ed12bdde42bde8a9f8b4550819040a066967
Parth Gupta <pagupta@chegg.com>
2025-03-25 01:28:53 +0530

    integrate H2 db for categories along with spring-boot-starter-data-jpa

--- src/main/java/com/ecommerce/web/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/web/service/CategoryServiceImpl.java
@@ -1,27 +1,27 @@
 package com.ecommerce.web.service;
 
 import com.ecommerce.web.model.Category;
+import com.ecommerce.web.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-    private final List<Category> categories = new ArrayList<>();
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(new Date().getTime());
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
@@ -30,11 +30,10 @@ public class CategoryServiceImpl implements CategoryService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category ID cannot be null");
         }
 
-        boolean removed = categories.removeIf(category -> category.getCategoryId().equals(categoryId));
+        Category categoryToDelete = categoryRepository.findById(categoryId).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
-        if (!removed) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
-        }
+        categoryRepository.delete(categoryToDelete);
     }
 
     public Category updateCategory(Long categoryId, Category updatedCategory) {
@@ -42,13 +41,11 @@ public class CategoryServiceImpl implements CategoryService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category ID or updated data cannot be null");
         }
 
-        Category existingCategory = categories.stream()
-                .filter(category -> category.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        Category categoryToUpdate = categoryRepository.findById(categoryId).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        categoryToUpdate.setCategoryName(updatedCategory.getCategoryName());
 
-        existingCategory.setCategoryName(updatedCategory.getCategoryName());
-
-        return existingCategory;
+        categoryRepository.save(categoryToUpdate);
+        return categoryToUpdate;
     }
 }
