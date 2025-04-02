422cdccd96e6f7781f0cdcf20c3f3dc99d90ea5c
Parth Gupta <pagupta@chegg.com>
2025-04-03 00:12:21 +0530

    add basic validations for ProductDTO

--- src/main/java/com/ecommerce/web/dto/request/ProductDTO.java
+++ src/main/java/com/ecommerce/web/dto/request/ProductDTO.java
@@ -1,5 +1,8 @@
 package com.ecommerce.web.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,15 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ProductDTO {
     private Long productId;
+    @NotBlank
+    @Size(min = 3, message = "Product name should be at least 3 characters long")
     private String productName;
+    @NotBlank
+    @Size(min = 10, message = "Product description should be at least 10 characters long")
     private String productDescription;
+    @NotNull
     private Double productPrice;
+    @NotNull
     private Integer productQuantity;
     private Double productDiscount;
     private Double productDiscountedPrice;
